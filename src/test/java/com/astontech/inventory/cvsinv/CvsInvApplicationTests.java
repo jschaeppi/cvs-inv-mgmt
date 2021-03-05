662ce78cabe2c11390fc478afd988a0a7d90891d
Joe <joseph.schaeppi@carlsonbuilding.com>
2021-03-04 18:49:58 -0600

    Added edit form for items.  Updated navigation. Finished the item part of the inventory management.  Added all the Vendor, Location, Address and Phone entities and services. Currently building reactive dynamic form Finished Vendor add form and almost complete with the Vendor edit form.

---
+++ src/test/java/com/astontech/inventory/cvsinv/CvsInvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astontech.inventory.cvsinv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvsInvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
